241211bfa737a78eba7870c97493abcce7eaa828
GitStudent2022 <torelgame@mail.ru>
2022-10-08 16:05:41 +0300

    Добавьте файлы проекта.

---
+++ CollectionProject/CollectionList.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionProject
+{
+    internal class CollectionList
+    {
+        static public void Run()
+        {
+            int[] array = new int[10];
+            // Array array = new int[10];
+
+            // Non generic collection ArrayList:
+
+            //ArrayList alist = new ArrayList();
+            //alist.Add(1);
+            //alist.Add("name");
+            //alist.AddRange(array);
+            //alist.Add(() => { Console.WriteLine("Hello"); });
+
+
+            //User user = new User();
+            //object obj = user;
+            //User? userNew = obj as User;
+
+
+            // List<int>
+
+            //List<int> list1 = new List<int>(10){ 6, 9, 2, 7, 4 };
+            //List<int> list2 = new List<int>(list1) { 6, 7, 8, 9 };
+            //List<int> list3 = new List<int>(10);
+
+            //int[] arrcopy = list1.ToArray();
+
+            //Console.WriteLine(list1.Exists(x => x % 2 == 0));
+            //Console.WriteLine(list1.Find(x => x % 2 == 0));
+            //Console.WriteLine(list1.FindLast(x => x % 2 == 0));
+            //var listRes1 = list1.FindAll(x => x % 2 == 0);
+            //foreach(var item in listRes1) Console.Write($"{item} ");
+
+
+
+            List<User> users = new List<User>()
+            {
+                new User(){ Name = "Bob", Age = 34 },
+                new User(){ Name = "Tom", Age = 22 },
+                new User(){ Name = "Joe", Age = 19 },
+                new User(){ Name = "Sam", Age = 41 },
+                new User(){ Name = "Tim", Age = 38 },
+                new User(){ Name = "Jim", Age = 25 },
+            };
+
+            foreach (var user in users) Console.WriteLine(user);
+            Console.WriteLine("--------------");
+
+            Console.WriteLine(users.Exists(u => u.Age < 16));
+            User? userFind = users.Find(u => u.Age < 20);
+            Console.WriteLine(userFind);
+            Console.WriteLine("--------------");
+
+            //users.RemoveAll(u => u.Age % 2 == 0);
+            //foreach (var user in users) Console.WriteLine(user);
+            //Console.WriteLine("--------------");
+
+            //userFind.Name = "Bill";
+            //foreach (var user in users) Console.WriteLine(user);
+            //Console.WriteLine("--------------");
+
+            var usersAgeEven = users.FindAll(u => u.Name[0] == 'J');
+            foreach (var user in usersAgeEven) Console.WriteLine(user);
+            Console.WriteLine("--------------");
+
+            users.Insert(3, new User() { Name = "Leo", Age = 27 });
+            foreach (var user in users) Console.WriteLine(user);
+            Console.WriteLine("--------------");
+
+            //users.Sort((u1, u2) => u1.Name.CompareTo(u2.Name));
+            users.Sort((u1, u2) => u1.Age - u2.Age);
+            //foreach (var user in users) Console.WriteLine(user);
+            //Console.WriteLine("--------------");
+
+            users.ForEach(u => Console.WriteLine(u));
+            Console.WriteLine("--------------");
+        }
+    }
+}
---
+++ CollectionProject/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CollectionProject
+{
+    class User
+    {
+        public string? Name { set; get; }
+        public int Age { set; get; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Age}";
+        }
+    }
+
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //CollectionList.Run();
+
+            List<User> users = new List<User>()
+            {
+                new User(){ Name = "Bob", Age = 34 },
+                new User(){ Name = "Tom", Age = 22 },
+                new User(){ Name = "Joe", Age = 19 },
+                new User(){ Name = "Sam", Age = 41 },
+                new User(){ Name = "Tim", Age = 38 },
+                new User(){ Name = "Jim", Age = 25 },
+                //new User(){ Name = "Sam", Age = 18 },
+            };
+
+            //LinkedList
+            //LinkedList<User> usersList = new LinkedList<User>(users);
+
+            //Console.WriteLine(usersList.Average(u => u.Age));
+            //Console.WriteLine(usersList.Sum(u => u.Age));
+            //Console.WriteLine(usersList.Min(u => u.Age));
+            //Console.WriteLine(usersList.Max(u => u.Age));
+
+
+            //Dictonary
+            //Dictionary<string, User> usersDict = new Dictionary<string, User>();
+
+            //foreach(var user in users)
+            //{
+            //    usersDict.TryAdd(user.Name, user);
+            //}
+            //Console.WriteLine("--------------");
+            //foreach (var key in usersDict.Keys)
+            //    Console.WriteLine($"{key} => {usersDict[key]}");
+            //Console.WriteLine("--------------");
+            //foreach (var value in usersDict.Values)
+            //    Console.WriteLine(value);
+            //Console.WriteLine("--------------");
+
+            //List<string> keys = new List<string>(usersDict.Keys);
+
+            foreach (var value in users)
+                Console.WriteLine(value);
+            Console.WriteLine("--------------");
+
+            //HashSet<User> usersHash = new HashSet<User>(users);
+            //foreach (var value in usersHash)
+            //    Console.WriteLine(value);
+            //Console.WriteLine("--------------");
+
+            //SortedList<string, User> usersSort = new SortedList<string, User>();
+            //foreach (var user in users)
+            //{
+            //    usersSort.TryAdd(user.Name, user);
+            //}
+            //foreach (var key in usersSort.Keys)
+            //    Console.WriteLine($"{key} => {usersSort[key]}");
+            //Console.WriteLine("--------------");
+
+
+            //Hashtable usersHT = new Hashtable();
+            //foreach (var user in users)
+            //{
+            //    usersHT.Add(user.GetHashCode(), user);
+            //}
+            //foreach (var key in usersHT.Keys)
+            //    Console.WriteLine($"{key} => {usersHT[key]}");
+            //Console.WriteLine("--------------");
+        }
+    }
+}
